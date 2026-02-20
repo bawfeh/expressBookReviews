f22d42378fa31384ed6529c484d04b7a20a5c92c
bawfeh <bawfeh@gmail.com>
2026-02-20 15:35:42 -0500

    Task10 done! Task 11--issues

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,22 +28,43 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  // Send JSON response with formatted list of books
-  return res.send(beautify(books));
+// Get the book list available in the shop [Asynchronous-Promise Callback]
+public_users.get('/', function (req, res) {
+    // Define the promise
+    const getAllBooks = new Promise((resolve, reject) => {
+        if (books) {
+            resolve(books);
+        } else {
+            reject("No books available");
+        }
+    });
+    // Call promise
+    getAllBooks.then(allBooks => {
+        res.send(beautify(allBooks));
+    }).catch(error => {
+        res.send(error + "\n");
+    });
+
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  // Retrieve books's details for the requested the ISBN parameter
-  const isbn = req.params.isbn;
-  if (books[isbn]) {
-    return res.send(beautify(books[isbn]));
-  } else {
-    return res.send(`No book with ISBN ${isbn}!\n`);
-  }
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const getBookByISBN = new Promise((resolve, reject) => {
+        if (books[isbn]) {
+            resolve(books[isbn]);
+        } else {
+            reject(`No book with ISBN ${isbn}!`);
+        }
+    });
+  
+    getBookByISBN.then(book => {
+        res.send(beautify(book));
+    }).catch(error => {
+        res.send(error + "\n");
+    });
+  
+  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
